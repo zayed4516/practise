39de58afde758896573c35ebee4f92a51d66eda8
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-18 21:04:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.header('file upload')
 file=st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
-  df=pd.read_csv(file)
+  df=pd.read_csv('housing.csv')
   st.write(df)
 num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
 names_col=st.multiselect('choose columns',df.columns.to_list())
