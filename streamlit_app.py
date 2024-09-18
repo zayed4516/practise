05346cdb7353de65c2aead475859e75ee3e99860
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-18 19:56:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+name=st.text_input("Enter your name")
+btn=st.button('show')
+
+if btn:
+    st.write(f'Welcome {name}')
