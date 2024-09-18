385123ea03e058fa985d7bba62e8d08064847501
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-18 19:56:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,17 @@ btn=st.button('show')
 
 if btn:
     st.write(f'Welcome {name}')
+
+#app 1
+st.header("calculate Area")
+choose =st.selectbox('Choose the shape',['circle','rectangle'])
+if choose=='circle':
+  r=st.number_input('enter the  radius',min_value=1,max_value=100)
+  area=r*r*3.14
+elif choose=='rectangle':
+     w=st.number_input('enter the  width',min_value=1,max_value=100)
+     h=st.number_input('enter the  height',min_value=1,max_value=100)
+     area=w*h
+btn=st.button('calculate')   
+if btn:  
+  st.write(f'Area is {area}')
