e47df907ae80aba38488a5308c42c814a40f30a7
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-18 20:23:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
-
 import pandas as pd
 st.header('file upload')
 file=st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
   df=pd.read_csv(file)
   st.write(df)
-    if btn:  
-     st.write(f'Area is {area}')
-if btn:  
-  st.write(f'Area is {area}')
