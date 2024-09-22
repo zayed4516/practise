88e04267d0748c083119396a565a7963ccbe4d31
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-22 20:54:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import time
 import pandas as pd
+import plotly.express as px
 st.header('file uploaded')
 uploaded_file=st.file_uploader('Choose the file',type=['csv'])
 if uploaded_file is not None:
   df=pd.read_csv(uploaded_file)
   n_rows=st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1,value=1)
   columns_choose=st.multiselect('Choose the columns',df.columns.to_list())
-  if columns_choose:
-    st.write(df[:n_rows][columns_choose])
-  else:
-    st.write(df[:n_rows])
+if columns_choose:
+  st.write(df[:n_rows][columns_choose])
+else:
+  st.write(df[:n_rows])
+fig=px.scatter(df,x='OrderID',y='OrderDetailID')
+st.plotly_chart(fig1)
